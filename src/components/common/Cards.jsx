61da1c60b8b2fc8a5fa7713b7580aaf6fe86fc0a
Marilyn Adjei <adjeimarilyn22@gmail.com>
2024-10-11 08:55:32 +0000

    update images path for vercel

--- src/components/common/Cards.jsx
+++ src/components/common/Cards.jsx
@@ -5,7 +5,7 @@ const Cards = () => {
     <div className="relative grid h-96 grid-rows-2 md:grid-rows-1 md:grid-cols-2 gap-5 my-7">
       <div className="relative ">
       <img
-          src="/src/assets/images/pexels-shu-lei-2809836-13849063.jpg"
+          src="/assets/images/pexels-shu-lei-2809836-13849063.jpg"
           alt="Workout Image"
           className="h-full w-full object-cover rounded-xl border-4 border-darkpink shadow-xl transition-all ease-in hover:drop-shadow-xl hover:-translate-y-3"/>
             <Link to="logworkout" className="absolute bottom-4 left-4 font-bold text-white text-2xl">Log Workout</Link>
@@ -13,7 +13,7 @@ const Cards = () => {
 
       <div className="relative ">
       <img
-          src="/src/assets/images/pexels-shu-lei-2809836-13849063.jpg"
+          src="/assets/images/pexels-shu-lei-2809836-13849063.jpg"
           alt="Workout Image"
           className="h-full w-full object-cover rounded-xl border-4 border-darkpink shadow-xl transition-all ease-in hover:drop-shadow-xl hover:-translate-y-3"/>
             <Link to="history" className="absolute bottom-4 left-4 font-bold text-white text-2xl">Workout History</Link>
